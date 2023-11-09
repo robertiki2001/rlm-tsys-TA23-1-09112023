259acc015efbabcffc486296dd93268e81c0df90
Robert <robertrlm16@gmail.com>
2023-11-09 16:17:15 +0100

    Tarea 23 proyecto Maven en Spring

---
+++ src/test/java/com/example/demo/Ta231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
